b962ee3075585bc4ab51668387e68ca67b4cbdf5
Edson Júnior <edsonjunior.1219@gmail.com>
2023-11-30 15:00:33 -0300

    feat(src/shared/repositories/account.repository.ts): implement new methods to account repository

--- src/shared/repositories/account.repository.ts
+++ src/shared/repositories/account.repository.ts
@@ -9,4 +9,20 @@ export class AccountRepository {
     create(createArgs: Prisma.AccountCreateArgs) {
         return this.prismaService.account.create(createArgs)
     }
+
+    findMany(findManyArgs: Prisma.AccountFindManyArgs) {
+        return this.prismaService.account.findMany(findManyArgs)
+    }
+
+    findFirst(findFirstArgs: Prisma.AccountFindFirstArgs) {
+        return this.prismaService.account.findFirst(findFirstArgs)
+    }
+
+    update(updateArgs: Prisma.AccountUpdateArgs) {
+        return this.prismaService.account.update(updateArgs)
+    }
+
+    delete(deleteArgs: Prisma.AccountDeleteArgs) {
+        return this.prismaService.account.delete(deleteArgs)
+    }
 }
