53f2a8bbbc55064c0e2f30d2be8f6f0109737ff0
Edson Júnior <edsonjunior.1219@gmail.com>
2023-11-30 14:53:05 -0300

    refac(src/app.module.ts): change user module name

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
-import { UsersModule } from './modules/users/users.module';
 import { DatabaseModule } from './shared/database/database.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { APP_GUARD } from '@nestjs/core';
 import { AuthGuard } from './modules/auth/auth.guard';
 import { AccountModule } from './modules/account/account.module';
+import { UserModule } from './modules/user/user.module';
 
 @Module({
-  imports: [UsersModule, DatabaseModule, AuthModule, AccountModule],
+  imports: [UserModule, DatabaseModule, AuthModule, AccountModule],
   controllers: [],
   providers: [
     {
