c6527ccdcb0036ddf74d90388583de1761758358
Edson Júnior <edsonjunior.1219@gmail.com>
2023-11-30 15:54:06 -0300

    feat(src/shared/database/database.module.ts): export category repository

--- src/shared/database/database.module.ts
+++ src/shared/database/database.module.ts
@@ -2,10 +2,11 @@ import { Global, Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
 import { UserRepository } from '../repositories/user.repository';
 import { AccountRepository } from '../repositories/account.repository';
+import { CategoryRepository } from '../repositories/category.repository';
 
 @Global()
 @Module({
-    providers: [PrismaService, UserRepository, AccountRepository],
-    exports: [UserRepository, AccountRepository]
+    providers: [PrismaService, UserRepository, AccountRepository, CategoryRepository],
+    exports: [UserRepository, AccountRepository, CategoryRepository]
 })
 export class DatabaseModule {}
