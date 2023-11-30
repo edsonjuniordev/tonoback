76d17450e11756a5db7db5c9f0e7fd527c848dd7
Edson Júnior <edsonjunior.1219@gmail.com>
2023-11-30 15:53:49 -0300

    feat(src/shared/repositories/category.repository.ts): create category repository

---
+++ src/shared/repositories/category.repository.ts
@@ -0,0 +1,16 @@
+import { Injectable } from "@nestjs/common";
+import { PrismaService } from "../database/prisma.service";
+import { Prisma } from "@prisma/client";
+
+@Injectable()
+export class CategoryRepository {
+    constructor(private readonly prismaService: PrismaService) { }
+
+    create(createArgs: Prisma.CategoryCreateArgs) {
+        return this.prismaService.category.create(createArgs);
+    }
+
+    findMany(findManyArgs: Prisma.CategoryFindManyArgs) {
+        return this.prismaService.category.findMany(findManyArgs);
+    }
+}
