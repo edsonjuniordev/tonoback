8dc7c8371be4e11a0a86234c2041c6c11586fa73
Edson Júnior <edsonjunior.1219@gmail.com>
2023-12-01 09:16:22 -0300

    feat(src/modules/transaction/transaction.service): create transaction service and test

---
+++ src/modules/transaction/transaction.service.spec.ts
@@ -0,0 +1,148 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { TransactionService } from './transaction.service';
+import { TransactionRepository } from '../../shared/repositories/transaction.repository';
+import { CreateTransactionDto } from './dto/create-transaction.dto';
+import { AccountRepository } from '../../shared/repositories/account.repository';
+import { CategoryRepository } from '../../shared/repositories/category.repository';
+
+const user = {
+  id: "e40e5a88-d12f-49fb-afeb-60e373f394a3",
+  email: "ed@email.com",
+  password: "$2a$12$uR09n2ZL/4HRRJLmPRWpEerUdGnjnD32//AHyWY5tQqoMaUxXsIMW",
+  name: "Ed",
+  social_login: false,
+  createdAt: "2023-11-15T23:20:11.476Z",
+  updatedAt: "2023-11-15T23:20:11.476Z",
+}
+
+const account = {
+  id: "e40e5a88-d12f-49fb-afeb-60e373f394a4",
+  userId: "e40e5a88-d12f-49fb-afeb-60e373f394a3",
+  name: "XP Investimentos",
+  balance: 100,
+  createdAt: new Date().toISOString(),
+  updatedAt: new Date().toISOString()
+}
+
+const category = {
+  id: "e40e5a88-d12f-49fb-afeb-60e373f394a4",
+  userId: "e40e5a88-d12f-49fb-afeb-60e373f394a3",
+  name: "Mercado"
+}
+
+const createTransactionDto = new CreateTransactionDto({
+  accountId: "e40e5a88-d12f-49fb-afeb-60e373f394a4",
+  categoryId: "e40e5a88-d12f-49fb-afeb-60e373f394a5",
+  type: "deposit",
+  description: "Deposito",
+  value: 100
+})
+
+const transaction = {
+  id: "d1982b14-e7b0-427f-8447-47d396b332ea",
+  userId: "e40e5a88-d12f-49fb-afeb-60e373f394a3",
+  accountId: "e904ebad-d348-4047-ae1f-3edb25bbac4d",
+  categoryId: "c0b3fd97-f714-4fa8-90e8-3d624d9328c6",
+  type: "in",
+  description: "Cashin",
+  value: 10,
+  createdAt: "2023-11-30T19:44:10.092Z"
+}
+
+describe('TransactionService', () => {
+  let transactionService: TransactionService;
+  let transactionRepository: TransactionRepository;
+  let accountRepository: AccountRepository;
+  let categoryRepository: CategoryRepository;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        TransactionService,
+        {
+          provide: TransactionRepository,
+          useValue: {
+            create: jest.fn().mockReturnValue(createTransactionDto),
+            findMany: jest.fn().mockReturnValue([transaction]),
+            findUnique: jest.fn().mockReturnValue(transaction)
+          },
+        },
+        {
+          provide: AccountRepository,
+          useValue: {
+            findUnique: jest.fn().mockResolvedValue(account),
+            update: jest.fn().mockResolvedValue(account),
+          }
+        },
+        {
+          provide: CategoryRepository,
+          useValue: {
+            findUnique: jest.fn().mockResolvedValue(category)
+          }
+        }
+      ],
+    }).compile();
+
+    transactionService = module.get<TransactionService>(TransactionService);
+    transactionRepository = module.get<TransactionRepository>(TransactionRepository);
+    accountRepository = module.get<AccountRepository>(AccountRepository);
+    categoryRepository = module.get<CategoryRepository>(CategoryRepository);
+  });
+
+  it('should be defined', () => {
+    expect(transactionService).toBeDefined();
+  });
+
+  describe("create", () => {
+    it("should create a new transaction", async () => {
+      // Act
+      const result = await transactionService.create(user.id, createTransactionDto);
+      // Assert
+      expect(result).toEqual(createTransactionDto);
+      expect(accountRepository.findUnique).toHaveBeenCalledTimes(1)
+      expect(categoryRepository.findUnique).toHaveBeenCalledTimes(1)
+      expect(transactionRepository.create).toHaveBeenCalledTimes(1);
+    })
+
+    it("should throw an exception", () => {
+      // Setup
+      jest.spyOn(accountRepository, "findUnique").mockRejectedValueOnce(new Error());
+      // Assert
+      expect(transactionService.create(user.id, createTransactionDto)).rejects.toThrow();
+    })
+  })
+
+  describe("findAllByUserId", () => {
+    it("should return all user transactions", async () => {
+      // Act
+      const result = await transactionService.findAllByUserId(user.id);
+      // Assert
+      expect(result).toEqual([transaction]);
+      expect(transactionRepository.findMany).toHaveBeenCalledTimes(1);
+    })
+
+    it("should throw an exception", () => {
+      // Setup
+      jest.spyOn(transactionRepository, "findMany").mockRejectedValueOnce(new Error());
+      // Assert
+      expect(transactionService.findAllByUserId(user.id)).rejects.toThrow();
+    })
+  })
+
+  describe("findOneByIdAndUserId", () => {
+    it("should return a user transaction", async () => {
+      // Act
+      const result = await transactionService.findOneByIdAndUserId(transaction.id, user.id);
+      // Assert
+      expect(result).toEqual(transaction);
+      expect(transactionRepository.findUnique).toHaveBeenCalledTimes(1);
+    })
+
+    it("should throw an exception", () => {
+      // Setup
+      jest.spyOn(transactionRepository, "findUnique").mockRejectedValueOnce(new Error());
+      // Assert
+      expect(transactionService.findOneByIdAndUserId(transaction.id, user.id)).rejects.toThrow();
+    })
+  })
+});
---
+++ src/modules/transaction/transaction.service.ts
@@ -0,0 +1,91 @@
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
+import { CreateTransactionDto } from './dto/create-transaction.dto';
+import { TransactionRepository } from '../../shared/repositories/transaction.repository';
+import { AccountRepository } from '../../shared/repositories/account.repository';
+import { CategoryRepository } from '../../shared/repositories/category.repository';
+
+@Injectable()
+export class TransactionService {
+  constructor(private readonly transactionRepository: TransactionRepository, private readonly accountRepository: AccountRepository, private readonly categoryRepository: CategoryRepository) { }
+
+  async create(userId: string, createTransactionDto: CreateTransactionDto) {
+    const { accountId, categoryId, description, type, value } = createTransactionDto
+
+    const isOwner = await this.accountRepository.findUnique({
+      where: {
+        id: accountId,
+        userId
+      }
+    })
+
+    if (!isOwner) {
+      throw new NotFoundException("account not found")
+    }
+
+    const categoryExists = await this.categoryRepository.findUnique({
+      where: {
+        id: categoryId
+      }
+    })
+
+    if (!categoryExists) {
+      throw new NotFoundException("category not found")
+    }
+
+    if (value > isOwner.balance) {
+      throw new BadRequestException("insuficient balance")
+    }
+
+    const transaction = await this.transactionRepository.create({
+      data: {
+        userId,
+        accountId,
+        categoryId,
+        description,
+        type,
+        value,
+        createdAt: new Date().toISOString(),
+      }
+    })
+
+    if (transaction) {
+      const transactionValue = type === "in" ? isOwner.balance + value : isOwner.balance - value
+
+      await this.accountRepository.update({
+        where: {
+          id: accountId
+        },
+        data: {
+          balance: transactionValue
+        }
+      })
+    } else {
+      throw new BadRequestException("transaction not created")
+    }
+
+    return transaction;
+  }
+
+  findAllByUserId(userId: string) {
+    return this.transactionRepository.findMany({
+      where: {
+        userId
+      }
+    });
+  }
+
+  findOneByIdAndUserId(transactionId: string, userId: string) {
+    const isOwner = this.transactionRepository.findUnique({
+      where: {
+        id: transactionId,
+        userId
+      }
+    })
+
+    if (!isOwner) {
+      throw new NotFoundException("transaction not found")
+    }
+
+    return isOwner;
+  }
+}
