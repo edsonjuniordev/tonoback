a4eb12621783e35054b4fe189ca7c29b7d1f75ad
Edson Júnior <edsonjunior.1219@gmail.com>
2023-12-01 09:13:56 -0300

    feat(src/app.module.ts): import transaction module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,10 @@ import { AuthGuard } from './modules/auth/auth.guard';
 import { AccountModule } from './modules/account/account.module';
 import { UserModule } from './modules/user/user.module';
 import { CategoryModule } from './modules/category/category.module';
+import { TransactionModule } from './modules/transaction/transaction.module';
 
 @Module({
-  imports: [UserModule, DatabaseModule, AuthModule, AccountModule, CategoryModule],
+  imports: [UserModule, DatabaseModule, AuthModule, AccountModule, CategoryModule, TransactionModule],
   controllers: [],
   providers: [
     {
