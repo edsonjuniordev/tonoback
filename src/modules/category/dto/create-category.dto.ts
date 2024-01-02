8e39ac228947d63b9ad3d72a9b6f2ae215b4ff3a
Edson Júnior <edsonjunior.1219@gmail.com>
2024-01-02 17:20:01 -0300

    feat(src/modules/category): add type

--- src/modules/category/dto/create-category.dto.ts
+++ src/modules/category/dto/create-category.dto.ts
@@ -1,11 +1,17 @@
 import { IsNotEmpty, IsString } from "class-validator";
+import { TransactionType } from "../../transaction/entities/transaction";
 
 export class CreateCategoryDto {
     constructor(createCategoryDto?: CreateCategoryDto) {
         this.name = createCategoryDto?.name;
+        this.type = createCategoryDto?.type
     }
 
     @IsString()
     @IsNotEmpty()
     name: string;
+
+    @IsString()
+    @IsNotEmpty()
+    type: TransactionType
 }
