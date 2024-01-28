86d078aa896671beb0beb0eda775f35f00a29d12
Edson Júnior <edsonjunior.1219@gmail.com>
2024-01-28 19:25:27 -0300

    feat(src/shared/config/env.ts): add new environment variables

--- src/shared/config/env.ts
+++ src/shared/config/env.ts
@@ -9,12 +9,19 @@ class Env {
     port: string;
 
     origin: string;
+
+    rateLimitTTL: number;
+
+    rateLimitRequests: number;
 }
 
 export const env: Env = plainToInstance(Env, {
     jwtSecret: process.env.JWT_SECRET,
     port: process.env.PORT,
-    origin: process.env.ORIGIN
+    origin: process.env.ORIGIN,
+    rateLimitTTL: Number(process.env.RATE_LIMIT_TTL),
+    rateLimitRequests: Number(process.env.RATE_LIMIT_REQUESTS)
 })
+
 const erros = validateSync(env)
 console.log(erros)
