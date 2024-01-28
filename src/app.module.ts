f39dd3503103feacfdb8663b2e1422c3fc6011ce
Edson Júnior <edsonjunior.1219@gmail.com>
2024-01-28 19:25:04 -0300

    feat(src/app.module.ts): configure rate limit

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,15 +7,24 @@ import { AccountModule } from './modules/account/account.module';
 import { UserModule } from './modules/user/user.module';
 import { CategoryModule } from './modules/category/category.module';
 import { TransactionModule } from './modules/transaction/transaction.module';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { env } from './shared/config/env';
 
 @Module({
-  imports: [UserModule, DatabaseModule, AuthModule, AccountModule, CategoryModule, TransactionModule],
+  imports: [UserModule, DatabaseModule, AuthModule, AccountModule, CategoryModule, TransactionModule, ThrottlerModule.forRoot([{
+    ttl: env.rateLimitTTL,
+    limit: env.rateLimitRequests,
+  }])],
   controllers: [],
   providers: [
     {
       provide: APP_GUARD,
       useClass: AuthGuard
+    },
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard
     }
   ],
 })
-export class AppModule {}
+export class AppModule { }
