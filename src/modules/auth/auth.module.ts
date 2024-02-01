bab51b93053eff6e2989be6cfa9898681584ba20
Edson Júnior <edsonjunior.1219@gmail.com>
2024-01-31 21:35:27 -0300

    feat(src/modules/auth): add account verification

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -3,6 +3,7 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt';
 import { env } from 'src/shared/config/env';
+import { EmailService } from '../email/email.service';
 
 @Module({
   imports: [
@@ -13,6 +14,6 @@ import { env } from 'src/shared/config/env';
     })
   ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, EmailService],
 })
 export class AuthModule { }
